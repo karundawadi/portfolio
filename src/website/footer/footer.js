d3853386adc5eb7d7575fd548ffe2c260ed198fc
Karun Dawadi <43080986+karundawadi@users.noreply.github.com>
2022-12-31 17:38:04 -0800

    Added spacing in footer section

--- src/website/footer/footer.js
+++ src/website/footer/footer.js
@@ -3,6 +3,7 @@ import {Typography, Box, Container } from "@mui/material"
 import LinkRow from "../navbar/link_row/link_row"
 
 function Footer(){
+    const breakPara = (customPadding="2.5%")=>{return <Box sx={{paddingTop:`${customPadding}`}}/>}
     return (
         <Container>
             <Box sx={{
@@ -13,6 +14,7 @@ function Footer(){
                     window.open("/")
                 }}>@Karun Dawadi</Typography>
                 <LinkRow/>
+                {breakPara("20px")}
             </Box>
         </Container>
     )
