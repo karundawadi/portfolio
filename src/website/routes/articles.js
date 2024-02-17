82ac0de85743176daea8e99388de39f1627f2eff
Karun Dawadi <43080986+karundawadi@users.noreply.github.com>
2024-02-17 14:15:14 -0800

    Lables introduced and minor bug fixes.

--- src/website/routes/articles.js
+++ src/website/routes/articles.js
@@ -1,104 +1,116 @@
-import { Container, Box, Typography } from "@mui/material";
+import { Container, Box, Typography, Button, Chip, Grid, TextField, TableContainer, TableCell, Table, TableRow } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import InputAdornment from "@mui/material/InputAdornment";
 import ProjectRow from "../navbar/project row/project_row";
 import Footer from "../footer/footer";
 import { Helmet } from "react-helmet";
-import { useNavigate } from "react-router-dom";
-import { TableRow, TableContainer, TableCell, Table } from "@mui/material";
-import React, { useState, useEffect } from "react";
-import { TextField, Grid } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import InputAdornment from "@mui/material/InputAdornment";
 import { AllBlogs } from "../bodyComponents/blogs/AllBlogs";
 
 function Articles(props) {
   const navigate = useNavigate();
   const [searchQuery, setSearchQuery] = useState("");
+  const [labelFilter, setLabelFilter] = useState("");
   const [displayedArticles, setDisplayedArticles] = useState(AllBlogs);
+  const [uniqueLabels, setUniqueLabels] = useState([]);
 
   useEffect(() => {
-    const lowercasedQuery = searchQuery.toLowerCase();
-    const filteredArticles = AllBlogs.filter((article) =>
-      article.data.title.toLowerCase().includes(lowercasedQuery)
+    let filteredArticles = AllBlogs.filter((article) =>
+      article.data.title.toLowerCase().includes(searchQuery.toLowerCase())
     );
 
-    // Sorting by date, newest first
+    if (labelFilter) {
+      filteredArticles = filteredArticles.filter((article) =>
+        article.data.labels.includes(labelFilter)
+      );
+    }
+
     const sortedArticles = filteredArticles.sort(
       (a, b) => new Date(b.data.date) - new Date(a.data.date)
     );
-    
+
     setDisplayedArticles(sortedArticles);
-  }, [searchQuery]);
+
+    // Exclude articles with hidden attribute set to true
+    const visibleArticles = AllBlogs.filter(article => !article.data.hidden);
+    const labels = visibleArticles.map(article => article.data.labels).flat();
+    setUniqueLabels([...new Set(labels)]);
+  }, [searchQuery, labelFilter]);
+
+  const handleLabelClick = (label) => {
+    setSearchQuery("");
+    setLabelFilter(label);
+  };
 
   return (
-    <Box style={{}}>
+    <Box>
       <Helmet>
         <title>Articles</title>
       </Helmet>
       <Container maxWidth="md">
-        <Typography
-          variant="h2"
-          onClick={() => {
-            window.open("/", "_self");
-          }}
-          fontWeight={335}
-          align={"center"}
-        >
+        <Typography variant="h2" onClick={() => { window.open("/", "_self"); }} fontWeight={335} align={"center"}>
           Karun Dawadi
         </Typography>
         <ProjectRow dark={props.dark} changeMode={props.changeMode} />
-        <Box sx={{ paddingTop: "2%" }}></Box>
-        <Grid container justifyContent="flex-end" sx={{ marginBottom: 2 }}>
-          <Grid item xs={12} sm={6} md={4}>
-            <TextField
-              label="Search articles..."
-              variant="outlined"
-              fullWidth
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SearchIcon />
-                  </InputAdornment>
-                ),
-                style: {
-                  height: "100%",
-                },
-              }}
-              autoComplete="false"
-              autoCorrect="false"
-              autoFocus="false"
-              name={`search-${Math.random()}`}
-              sx={{
-                "& .MuiOutlinedInput-root": {
-                  "& fieldset": {
-                    borderTopRightRadius: 0,
-                    borderBottomRightRadius: 0,
-                  },
-                },
-                "& .MuiInputLabel-root": {
-                  lineHeight: "1",
-                },
-                "& .MuiOutlinedInput-input": {
-                  padding: "10px 14px",
-                },
-                "& .MuiInputAdornment-root": {
-                  marginTop: "0px", // Adjust to align properly with the text if needed
-                },
-              }}
-            />
+        <Box sx={{ pt: 2 }}>
+          <Grid container spacing={2} alignItems="center" justifyContent="space-between">
+            <Grid item xs={12} sm={true} sx={{ display: 'flex', justifyContent: 'center', gap: '10px', flexWrap: 'wrap', pt: 1 }}>
+              <Box sx={{ display: 'flex', justifyContent: 'center', flexGrow: 1, gap: '10px', flexWrap: 'wrap' }}>
+                {uniqueLabels.map((label) => (
+                  <Chip
+                    key={label}
+                    label={label}
+                    onClick={() => handleLabelClick(label)}
+                    style={{ margin: '5px' }}
+                    variant={label === labelFilter ? "outlined" : "default"}
+                  />
+                ))}
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4} md={4} lg={4} sx={{ display: 'flex', justifyContent: 'flex-end', pt: 1 }}>
+              <TextField
+                label="Search articles..."
+                variant="outlined"
+                fullWidth
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SearchIcon />
+                    </InputAdornment>
+                  ),
+                  style: { height: "100%" },
+                }}
+                autoComplete="off"
+                autoCorrect="off"
+                name={`search-${Math.random()}`}
+              />
+            </Grid>
           </Grid>
-        </Grid>
+        </Box>
+
+        {labelFilter && (
+          <Box sx={{ mb: 2, paddingTop: '2%' }}>
+            <Button variant="contained" color="info" onClick={() => setLabelFilter("")}   sx={{
+    fontSize: '0.875rem',
+    padding: '6px 16px',
+    borderRadius: '4px',
+    textTransform: 'none',
+    margin: '8px 0', // Adjust as needed for your layout
+    '&:hover': {
+      backgroundColor: 'primary.dark', // Adjust for a darker shade on hover
+    },
+  }}>
+              Clear label filter: {labelFilter}
+            </Button>
+          </Box>
+        )}
         <TableContainer>
           <Table>
-            {displayedArticles
-            .filter(article => !article.data.hidden)
-            .map((article) => (
-              <TableRow
-                key={article.path}
-                hover
-                onClick={() => navigate(article.path)}
-              >
+            {displayedArticles.filter(article => !article.data.hidden).map((article) => (
+              <TableRow hover key={article.path} onClick={() => navigate(article.path)}>
                 <TableCell align="left">{article.data.title}</TableCell>
                 <TableCell align="right">
                   {new Date(article.data.date).toLocaleDateString("en-US", {
